fa088fe6dc3dda23dae4d3f51db09b32cff84472
Derick Rethans <derick@php.net>
2012-03-02 17:06:45 +0000

    - Released version 2012.2 (2012b)
    git-svn-id: http://svn.php.net/repository/pecl/timezonedb/tags/RELEASE_2012_2@323832 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ trunk/php_timezonedb.h
@@ -0,0 +1,40 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2005 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.0 of the PHP license,       |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_0.txt.                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors: Derick Rethans <derick@php.net>                             |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id$ */
+
+#ifndef PHP_TIMEZONEDB_H
+#define PHP_TIMEZONEDB_H
+
+#include "php.h"
+
+extern zend_module_entry timezonedb_module_entry;
+#define phpext_timezonedb_ptr &timezonedb_module_entry
+
+PHP_MINIT_FUNCTION(timezonedb);
+PHP_MINFO_FUNCTION(timezonedb);
+
+#endif	/* TIMEZONEDB_H */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * indent-tabs-mode: t
+ * End:
+ */
