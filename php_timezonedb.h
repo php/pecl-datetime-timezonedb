da24991755e3edf0fb9c772ddcc1a97dce1bf2d9
Derick Rethans <derick@php.net>
2015-04-24 16:06:31 +0000

    - Updated to version 2015.4 (2015d)
    git-svn-id: http://svn.php.net/repository/pecl/timezonedb/trunk@336651 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_timezonedb.h
+++ php_timezonedb.h
@@ -26,7 +26,7 @@
 extern zend_module_entry timezonedb_module_entry;
 #define phpext_timezonedb_ptr &timezonedb_module_entry
 
-#define PHP_TIMEZONEDB_VERSION "2015.3"
+#define PHP_TIMEZONEDB_VERSION "2015.4"
 
 PHP_MINIT_FUNCTION(timezonedb);
 PHP_MINFO_FUNCTION(timezonedb);
