e283a74df00a8caa66f4827ff04afcb3c0788754
Derick Rethans <github@derickrethans.nl>
2022-10-12 09:22:55 +0100

    Updated to version 2022.5 (2022e)

--- php_timezonedb.h
+++ php_timezonedb.h
@@ -26,7 +26,7 @@
 extern zend_module_entry timezonedb_module_entry;
 #define phpext_timezonedb_ptr &timezonedb_module_entry
 
-#define PHP_TIMEZONEDB_VERSION "2022.4"
+#define PHP_TIMEZONEDB_VERSION "2022.5"
 
 PHP_MINIT_FUNCTION(timezonedb);
 PHP_MINFO_FUNCTION(timezonedb);
